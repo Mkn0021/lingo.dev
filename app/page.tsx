fdf8d4dea08e6342cea25044fd09a8a9c9e00749
Mkn0021 <nayeem21122002@gmail.com>
2026-02-17 23:02:02 +0600

    chore: add shadcn and preetier config

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center">
-      Hello Ligo Dev
-    </div>
-  );
+	return (
+		<div className="flex min-h-screen items-center justify-center">
+			Hello Ligo Dev
+		</div>
+	);
 }
